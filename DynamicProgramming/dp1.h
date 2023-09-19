591a054173c2f200f90c2233a97b3e68971e3a5a
ehddnr <ehddnrdlaos@gmail.com>
2023-09-19 21:22:18 +0900

    Update/0919 memoization

--- DynamicProgramming/dp1.h
+++ DynamicProgramming/dp1.h
@@ -5,112 +5,116 @@
 #include <map>
 #include <string>
 
-//재귀호출
-//	트리로 표현하면 이해하기 쉽다.
-//	시간복잡도 : 전체 노드의 갯수
-//	공간복잡도 : 트리의 높이
 
-//BruteForce - 전수조사
-// TC : O(2^n)
-// SC : O(n)
-int64_t Fibonacci(int n)
+namespace dp_base
 {
-	//base case
-	if (n<=2)
+	//재귀호출
+	//	트리로 표현하면 이해하기 쉽다.
+	//	시간복잡도 : 전체 노드의 갯수
+	//	공간복잡도 : 트리의 높이
+
+	//BruteForce - 전수조사
+	// TC : O(2^n)
+	// SC : O(n)
+	int64_t Fibonacci(int n)
 	{
-		return 1;
+		//base case
+		if (n <= 2)
+		{
+			return 1;
+		}
+
+		//recursive
+		return Fibonacci(n - 1) + Fibonacci(n - 2);
 	}
 
-	//recursive
-	return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
-
-// Optimize - 중복계산 제거
-//		문제를 풀고 결과를 저장한다음에 재사용
-// TC : O(n)
-// SC : O(n)
-int64_t Fibonacci_memo(int n, std::map<int, int64_t>& memo)
-{
-	if (memo.count(n) == 1)
+	// Optimize - 중복계산 제거
+	//		문제를 풀고 결과를 저장한다음에 재사용
+	// TC : O(n)
+	// SC : O(n)
+	int64_t Fibonacci_memo(int n, std::map<int, int64_t>& memo)
 	{
+		if (memo.count(n) == 1)
+		{
+			return memo[n];
+		}
+
+		//base case
+		if (n <= 2)
+		{
+			return 1;
+		}
+
+		//recursive
+		memo[n] = Fibonacci_memo(n - 1, memo) + Fibonacci_memo(n - 2, memo);
 		return memo[n];
 	}
 
-	//base case
-	if (n <= 2)
+
+	//TC : O(2^(m+n))
+	//SC : O(m+n)
+	int FindWay(int m, int n)
 	{
-		return 1;
+		//base case
+		if (m == 1 && n == 1)
+		{
+			return 1;
+		}
+		if (m == 0 || n == 0)
+		{
+			return 0;
+		}
+
+		//recursive case
+		return FindWay(m - 1, n) + FindWay(m, n - 1);
 	}
 
-	//recursive
-	memo[n] = Fibonacci_memo(n - 1,memo) + Fibonacci_memo(n - 2,memo);
-	return memo[n];
-}
 
+	//DynamicProgramming (동적계획법)
+	//	memoization(메모화)
+	//		문제의 답을 메모에 적어놓고 재활용
+	//		
+	//	1. 문제를 푼다 - BruteForce
+	//		a. 문제를 tree 구조로 표현해 본다.
+	//		b. 재귀호출로 구현
+	//
+	//	2. Memoization
+	//		a. 공통된 패턴을 찾는다.
+	//		b. 저장하고 재사용 - 메모
+	//
+	//	3. 테스트
+	//		a. 1,2 의 경과가 같아야 한다.
+	//		b. 1번의 TestCase(입력/출력 예제)를 2번에 적용
 
-//TC : O(2^(m+n))
-//SC : O(m+n)
-int FindWay(int m, int n)
-{
-	//base case
-	if (m == 1 && n == 1)
+	// TC : O(n) / O(m*n)
+	// SC : O(m+n)
+	int FindWay2(int m, int n, std::map<std::string, int>& memo)
 	{
-		return 1;
-	}
-	if (m == 0 || n == 0)
-	{
-		return 0;
-	}
+		std::string key = std::to_string(m) + "," + std::to_string(n);
+		if (memo.count(key) == 1)
+		{
+			return memo[key];
+		}
 
-	//recursive case
-	return FindWay(m - 1, n) + FindWay(m, n - 1);
-}
+		std::string rkey = std::to_string(n) + "," + std::to_string(m);
+		if (memo.count(rkey) == 1)
+		{
+			return memo[rkey];
+		}
 
 
-//DynamicProgramming (동적계획법)
-//	memoization(메모화)
-//		문제의 답을 메모에 적어놓고 재활용
-//		
-//	1. 문제를 푼다 - BruteForce
-//		a. 문제를 tree 구조로 표현해 본다.
-//		b. 재귀호출로 구현
-//
-//	2. Memoization
-//		a. 공통된 패턴을 찾는다.
-//		b. 저장하고 재사용 - 메모
-//
-//	3. 테스트
-//		a. 1,2 의 경과가 같아야 한다.
-//		b. 1번의 TestCase(입력/출력 예제)를 2번에 적용
+		//base case
+		if (m == 1 && n == 1)
+		{
+			return 1;
+		}
+		if (m == 0 || n == 0)
+		{
+			return 0;
+		}
 
-// TC : O(n) / O(m*n)
-// SC : O(m+n)
-int FindWay2(int m, int n, std::map<std::string, int>& memo)
-{
-	std::string key = std::to_string(m) + "," + std::to_string(n);
-	if (memo.count(key) == 1)
-	{
+		//recursive case
+		memo[key] = FindWay2(m - 1, n, memo) + FindWay2(m, n - 1, memo);
 		return memo[key];
 	}
-
-	std::string rkey = std::to_string(n) + "," + std::to_string(m);
-	if (memo.count(rkey) == 1)
-	{
-		return memo[rkey];
-	}
-
-
-	//base case
-	if (m == 1 && n == 1)
-	{
-		return 1;
-	}
-	if (m == 0 || n == 0)
-	{
-		return 0;
-	}
-
-	//recursive case
-	memo [key] = FindWay2(m - 1, n,memo) + FindWay2(m, n - 1,memo);
-	return memo[key];
 }
--- DynamicProgramming/dp2.h
+++ DynamicProgramming/dp2.h
@@ -1,7 +1,9 @@
 #pragma once
-#include <iostream>
+
 #include <vector>
 #include <map>
+#include <list>
+#include <memory>
 
 //*Decision Problem
 //	합을 구할 수 있느냐?
@@ -12,260 +14,348 @@
 //***Optimization Problem
 //	가장 짧은 배열은?
 //		ㄴ모든 경우를 구해야함
-
-//BruteForce
-// m : sum, n : numbers의 크기
-// TC : O(n^m)
-// SC : O(m)
-bool CanAccumulate(const std::vector<int>& numbers, int sum)
+namespace memoization
 {
-	//base case
-	if (sum == 0)
-	{
-		return true;
-	}
-	if (sum < 0)
-	{
-		return false;
-	}
 
-	//recursive
-	for (auto e : numbers)
+	std::ostream& operator << (std::ostream& os, std::vector<int>* v)
 	{
-		if (CanAccumulate(numbers, sum - e))
+		if (v == nullptr)
 		{
-			return true;
+			std::cout << "nullptr";
+			return os;
 		}
-	}
-	return false;
-}
 
-
-//Memoization
-// m : sum, n : numbers의 크기
-// TC : O(m*n)
-// SC : O(m)
-bool CanAccumulate2(const std::vector<int>& numbers, int sum, std::map<int,bool>& memo)
-{
-	if (memo.count(sum) == 1)
-	{
-		return memo[sum];
-	}
-
-	//base case
-	if (sum == 0)
-	{
-		return true;
-	}
-	if (sum < 0)
-	{
-		return false;
-	}
-
-	//recursive
-	for (auto e : numbers)
-	{
-		if (CanAccumulate2(numbers, sum - e,memo))
+		std::cout << "{";
+		for (int i = 0; i < v->size(); i++)
 		{
-			memo[sum] = true;
-			return true;
-		}
-	}
-
-	memo[sum] = false;
-	return false;
-}
-
-//Memoization
-// TC : O(m * n)
-// SC : O(m^2),	
-//		O(m) - 입력 출력은 복잡도에서 제외할 경우
-using result = std::shared_ptr<std::vector<int>>;
-result HowAccumulate(const std::vector<int>& numbers, int sum, std::map<int, result>& memo)
-{
-	if (memo.count(sum) == 1)
-	{
-		return memo[sum];
-	}
-
-	// base case
-	if (sum == 0)
-	{
-		return std::make_shared<std::vector<int>>(); // {}
-	}
-	if (sum < 0)
-	{
-		return nullptr;
-	}
-
-	//recursive case
-	for (const auto e : numbers)
-	{
-		auto r = HowAccumulate(numbers, sum - e, memo);
-		if (r != nullptr)
-		{
-			r->push_back(e);
-			memo[sum] = r;
-			return memo[sum];
-		}
-	}
-	memo[sum] = nullptr;
-	return memo[sum];
-}
-
-//최소의 크기 : m = sum, n = numbers의 크기
-// TC : O(m * n)
-// //	std::copy 시간 고려 0(m^2 * n)
-// SC : O(m ^ 2)
-result OptimizeAccumulate(const std::vector<int>& numbers, int sum, std::map<int, result>& memo)
-{
-	if (memo.count(sum) == 1)
-	{
-		return memo[sum];
-	}
-	//base case
-	if (sum == 0)
-	{
-		return std::make_shared<std::vector<int>>();
-	}
-	if (sum < 0)
-	{
-		return nullptr;
-	}
-
-	//recursive case
-	std::shared_ptr<std::vector<int>> optimized = nullptr;
-
-	for (const auto e : numbers)
-	{
-		auto r = OptimizeAccumulate(numbers, sum - e, memo);
-
-		if (r != nullptr)
-		{
-			std::shared_ptr<std::vector<int>> v = std::make_shared<std::vector<int>>();
-			v->resize(r->size());
-			std::copy(r->begin(), r->end(), v->begin());
-
-			r->push_back(e);
-
-			if (optimized == nullptr || v->size() < optimized->size())
+			std::cout << (*v)[i];
+			if (i < v->size() - 1)
 			{
-				optimized = v;
+				std::cout << ",";
 			}
 		}
-	}
-	memo[sum] = optimized;
-	return memo[sum];
-}
-
-// m : target, n:strings의 크기
-// TC : O(n^m *m)
-// SC : O(m ^ 2)
-bool CanGenerate(const std::vector<std::string>& strings, std::string target)
-{
-	// base case
-	if (target == "")
-	{
-		return true;
+		std::cout << "}";
+		return os;
 	}
 
-	// recursive
-	for (auto e : strings)
+
+	//BruteForce
+	// m : sum, n : numbers의 크기
+	// TC : O(n^m)
+	// SC : O(m)
+
+	bool CanAccumulate(const std::vector<int>& numbers, int sum)
 	{
-		if (target.find(e) == 0)
+		//base case
+		if (sum == 0)
 		{
-			auto subStr = target.substr(e.size());
-			if (CanGenerate(strings, subStr))
+			return true;
+		}
+		if (sum < 0)
+		{
+			return false;
+		}
+
+		//recursive
+		for (auto e : numbers)
+		{
+			if (CanAccumulate(numbers, sum - e))
 			{
 				return true;
 			}
 		}
-	}
-	return false;
-}
-
-// Memoization
-// TC : O(m^2 * n)
-// SC : O(m^2)
-bool CanGenerate(const std::vector<std::string>& strings, std::string target, std::map<std::string, bool>& memo)
-{
-	if (memo.count(target) == 1)
-	{
-		return memo[target];
+		return false;
 	}
 
-	// base case
-	if (target == "")
-	{
-		return true;
-	}
 
-	// recursive
-	for (auto e : strings)
+	//Memoization
+	// m : sum, n : numbers의 크기
+	// TC : O(m*n)
+	// SC : O(m)
+	bool CanAccumulate2(const std::vector<int>& numbers, int sum, std::map<int, bool>& memo)
 	{
-		if (target.find(e) == 0)
+		if (memo.count(sum) == 1)
 		{
-			auto subStr = target.substr(e.size());
-			if (CanGenerate(strings, subStr))
+			return memo[sum];
+		}
+
+		//base case
+		if (sum == 0)
+		{
+			return true;
+		}
+		if (sum < 0)
+		{
+			return false;
+		}
+
+		//recursive
+		for (auto e : numbers)
+		{
+			if (CanAccumulate2(numbers, sum - e, memo))
 			{
-				memo[target] = true;
-				return memo[target];
+				memo[sum] = true;
+				return true;
 			}
 		}
-	}
-	memo[target] = false;
-	return memo[target];
-}
 
-// Combination - Brute Force
-// TC : O(n^m *m)
-// SC : O(m^2)
-int HowManyGenerate(const std::vector<std::string>& strings, std::string target)
-{
-	// base case
-	if (target == "")
-	{
-		return 1;
+		memo[sum] = false;
+		return false;
 	}
 
-	//recursive case
-	int count{};
-	for (auto e : strings)
+	//Memoization
+	// TC : O(m * n)
+	// SC : O(m^2),	
+	//		O(m) - 입력 출력은 복잡도에서 제외할 경우
+	using result = std::shared_ptr<std::vector<int>>;
+	result HowAccumulate(const std::vector<int>& numbers, int sum, std::map<int, result>& memo)
 	{
-		if (target.find(e) == 0)
+		if (memo.count(sum) == 1)
 		{
-			count += HowManyGenerate(strings, target.substr(e.size()));
+			return memo[sum];
 		}
-	}
-	return count;
-}
 
-// Combination - Memoization
-// TC : O()
-// SC : O()
-int HowManyGenerate(const std::vector<std::string>& strings, std::string target, std::map<std::string, int>& memo)
-{
-	if (memo.count(target) == 1)
+		// base case
+		if (sum == 0)
+		{
+			return std::make_shared<std::vector<int>>(); // {}
+		}
+		if (sum < 0)
+		{
+			return nullptr;
+		}
+
+		//recursive case
+		for (const auto e : numbers)
+		{
+			auto r = HowAccumulate(numbers, sum - e, memo);
+			if (r != nullptr)
+			{
+				r->push_back(e);
+				memo[sum] = r;
+				return memo[sum];
+			}
+		}
+		memo[sum] = nullptr;
+		return memo[sum];
+	}
+
+	//최소의 크기 : m = sum, n = numbers의 크기
+	// TC : O(m * n)
+	// //	std::copy 시간 고려 0(m^2 * n)
+	// SC : O(m ^ 2)
+	result OptimizeAccumulate(const std::vector<int>& numbers, int sum, std::map<int, result>& memo)
 	{
+		if (memo.count(sum) == 1)
+		{
+			return memo[sum];
+		}
+		//base case
+		if (sum == 0)
+		{
+			return std::make_shared<std::vector<int>>();
+		}
+		if (sum < 0)
+		{
+			return nullptr;
+		}
+
+		//recursive case
+		std::shared_ptr<std::vector<int>> optimized = nullptr;
+
+		for (const auto e : numbers)
+		{
+			auto r = OptimizeAccumulate(numbers, sum - e, memo);
+
+			if (r != nullptr)
+			{
+				std::shared_ptr<std::vector<int>> v = std::make_shared<std::vector<int>>();
+				v->resize(r->size());
+				std::copy(r->begin(), r->end(), v->begin());
+
+				r->push_back(e);
+
+				if (optimized == nullptr || v->size() < optimized->size())
+				{
+					optimized = v;
+				}
+			}
+		}
+		memo[sum] = optimized;
+		return memo[sum];
+	}
+
+	// m : target, n:strings의 크기
+	// TC : O(n^m *m)
+	// SC : O(m ^ 2)
+	bool CanGenerate(const std::vector<std::string>& strings, std::string target)
+	{
+		// base case
+		if (target == "")
+		{
+			return true;
+		}
+
+		// recursive
+		for (auto e : strings)
+		{
+			if (target.find(e) == 0)
+			{
+				auto subStr = target.substr(e.size());
+				if (CanGenerate(strings, subStr))
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	// Memoization
+	// TC : O(m^2 * n)
+	// SC : O(m^2)
+	bool CanGenerate(const std::vector<std::string>& strings, std::string target, std::map<std::string, bool>& memo)
+	{
+		if (memo.count(target) == 1)
+		{
+			return memo[target];
+		}
+
+		// base case
+		if (target == "")
+		{
+			return true;
+		}
+
+		// recursive
+		for (auto e : strings)
+		{
+			if (target.find(e) == 0)
+			{
+				auto subStr = target.substr(e.size());
+				if (CanGenerate(strings, subStr))
+				{
+					memo[target] = true;
+					return memo[target];
+				}
+			}
+		}
+		memo[target] = false;
 		return memo[target];
 	}
 
-	// base case
-	if (target == "")
+	// Combination - Brute Force
+	// TC : O(n^m *m)
+	// SC : O(m^2)
+	int HowManyGenerate(const std::vector<std::string>& strings, std::string target)
 	{
-		return 1;
-	}
-
-	//recursive case
-	int count{};
-	for (auto e : strings)
-	{
-		if (target.find(e) == 0)
+		// base case
+		if (target == "")
 		{
-			count += HowManyGenerate(strings, target.substr(e.size()),memo);
+			return 1;
 		}
+
+		//recursive case
+		int count{};
+		for (auto e : strings)
+		{
+			if (target.find(e) == 0)
+			{
+				count += HowManyGenerate(strings, target.substr(e.size()));
+			}
+		}
+		return count;
 	}
 
-	memo[target] = count;
-	return memo[target];
+	// Combination - Memoization
+	// TC : O()
+	// SC : O()
+	int HowManyGenerate(const std::vector<std::string>& strings, std::string target, std::map<std::string, int>& memo)
+	{
+		if (memo.count(target) == 1)
+		{
+			return memo[target];
+		}
+
+		// base case
+		if (target == "")
+		{
+			return 1;
+		}
+
+		//recursive case
+		int count{};
+		for (auto e : strings)
+		{
+			if (target.find(e) == 0)
+			{
+				count += HowManyGenerate(strings, target.substr(e.size()), memo);
+			}
+		}
+
+		memo[target] = count;
+		return memo[target];
+	}
+
+	using string2d = std::list<std::list<std::string>>;
+
+	std::ostream& operator << (std::ostream& os, string2d v)
+	{
+		std::cout << "{" << std::endl;
+		int i{}, j{};
+
+		for (auto e1 : v)
+		{
+			j = 0;
+			std::cout << "    {";
+			for (auto e2 : e1)
+			{
+				std::cout << e2;
+
+				if (j < e1.size() - 1)
+				{
+					std::cout << ",";
+				}
+				j++;
+			}
+			std::cout << "}";
+
+			if (i < v.size() - 1)
+			{
+				std::cout << ",";
+			}
+			std::cout << std::endl;
+			i++;
+		}
+		std::cout << "}" << std::endl;
+
+		return os;
+	}
+
+	// TC : O(n^m * m)
+	// SC : O(m^2)
+	string2d AllCombination(const std::vector<std::string>& strings, std::string target)
+	{
+		//base case
+		if (target == "")
+		{
+			return string2d{ {} };
+		}
+
+		//recursive case
+		string2d v;
+		for (auto word : strings)
+		{
+			if (target.find(word) == 0)
+			{
+				auto r = AllCombination(strings, target.substr(word.size()));
+				for (auto s : r)
+				{
+					s.push_front(word);
+					v.push_front(s);
+				}
+			}
+		}
+		return v;
+	}
 }
